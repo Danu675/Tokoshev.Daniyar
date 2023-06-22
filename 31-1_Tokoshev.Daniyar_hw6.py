14273339fb9487826ebdc224688d215e7e446ff2
Danu675 <tokoshev19@mail.ru>
2023-06-22 23:32:34 +0600

    HW6

--- 31-1_Tokoshev.Daniyar_hw6.py
+++ 31-1_Tokoshev.Daniyar_hw6.py
@@ -4,21 +4,32 @@ def bubble_sort(arr):
         for j in range(n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    print(arr)
-
+    return arr
 
 def binary_search(element, arr):
-    low = 0
-    high = len(arr) - 1
-    while low <= high:
-        mid = (low + high) // 2
+    first = 0
+    last = len(arr) - 1
+    resultOK = False
+    result = None
+    while first < last:
+        mid = (first + last) // 2
+
         if arr[mid] == element:
-            print(mid)
+            first = last = mid
+            resultOK = True
+            result = mid
             break
         elif arr[mid] < element:
-            low = mid + 1
+            first = mid + 1
         else:
-            high = mid - 1
+            last = mid - 1
+
+    if resultOK:
+        print(f"Элемент найден на позиции {result}.")
+    else:
+        print("Элемент не найден.")
+
+    print("Конец")
 
 
 bubble_sort([1, 2, 2, 2, 3, 1, 4, 3])
